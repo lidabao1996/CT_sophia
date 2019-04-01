0bc6dce1422f9305f63d8b3dcbe6d5451502ec15
lifang <12345678>
2019-04-01 16:55:03 +0800

    大数据技术之Flume

---
+++ ct_product/src/main/java/ProductLog.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductLog {
+    private String startTime = "2018-01-01";
+    private String endTime = "2018-12-31";
+
+    //生产数据
+    //存放电话号码
+    private List<String> phoneList = new ArrayList<>();
+    //存放电话(号码,人)
+    private Map<String, String> phoneNameMap = new HashMap();
+
+    public void initPhone() {
+        //20个电话号码
+        phoneList.add("17078388295");
+        phoneList.add("13980337439");
+        phoneList.add("14575535933");
+        phoneList.add("19902496992");
+        phoneList.add("18549641558");
+        phoneList.add("17005930322");
+        phoneList.add("18468618874");
+        phoneList.add("18576581848");
+        phoneList.add("15978226424");
+        phoneList.add("15542823911");
+        phoneList.add("17526304161");
+        phoneList.add("15422018558");
+        phoneList.add("17269452013");
+        phoneList.add("17764278604");
+        phoneList.add("15711910344");
+        phoneList.add("15714728273");
+        phoneList.add("16061028454");
+        phoneList.add("16264433631");
+        phoneList.add("17601615878");
+        phoneList.add("15897468949");
+
+        //20个号码 + 人
+        phoneNameMap.put("17078388295", "李雁");
+        phoneNameMap.put("13980337439", "卫艺");
+        phoneNameMap.put("14575535933", "仰莉");
+        phoneNameMap.put("19902496992", "陶欣悦");
+        phoneNameMap.put("18549641558", "施梅梅");
+        phoneNameMap.put("17005930322", "金虹霖");
+        phoneNameMap.put("18468618874", "魏明艳");
+        phoneNameMap.put("18576581848", "华贞");
+        phoneNameMap.put("15978226424", "华啟倩");
+        phoneNameMap.put("15542823911", "仲采绿");
+        phoneNameMap.put("17526304161", "卫丹");
+        phoneNameMap.put("15422018558", "戚丽红");
+        phoneNameMap.put("17269452013", "何翠柔");
+        phoneNameMap.put("17764278604", "钱溶艳");
+        phoneNameMap.put("15711910344", "钱琳");
+        phoneNameMap.put("15714728273", "缪静欣");
+        phoneNameMap.put("16061028454", "焦秋菊");
+        phoneNameMap.put("16264433631", "吕访琴");
+        phoneNameMap.put("17601615878", "沈丹");
+        phoneNameMap.put("15897468949", "褚美丽");
+
+
+    }
+}
