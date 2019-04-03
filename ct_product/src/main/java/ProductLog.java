2c7eb40c7afb981fd4635461cfa452153d738508
lifang <12345678>
2019-04-03 13:57:46 +0800

    产生随机数据

--- ct_product/src/main/java/ProductLog.java
+++ ct_product/src/main/java/ProductLog.java
@@ -1,7 +1,9 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ProductLog {
     private String startTime = "2018-01-01";
@@ -58,6 +60,100 @@ public class ProductLog {
         phoneNameMap.put("17601615878", "沈丹");
         phoneNameMap.put("15897468949", "褚美丽");
 
+
+    }
+
+
+    public String product() {
+        //主叫
+        String caller = null;
+        //被叫
+        String callee = null;
+        //主叫姓名
+        String callerName = null;
+        //被叫姓名
+        String calleeName = null;
+        //随机索引
+
+        //取得主叫号码
+        int callerIndex = (int) (Math.random() * phoneList.size());
+        caller = phoneList.get(callerIndex);
+        callerName = phoneNameMap.get(caller);
+
+        while (true) {
+            //取得被叫号码
+            int calleeIndex = (int) (Math.random() * phoneList.size());
+            callee = phoneList.get(calleeIndex);
+            calleeName = phoneNameMap.get(callee);
+            if (!caller.equals(callee)) break;
+        }
+
+        //随机建立通话时间
+        String buildTime = randomBuildTime(startTime, endTime);
+        //规定通话时长的格式化限制
+        DecimalFormat df = new DecimalFormat("0000");
+        String duration = df.format((int) (30 * 60 * Math.random()));
+        StringBuffer sb = new StringBuffer();
+        sb.append(caller + ",").append(callee + ",").append(buildTime + ",").append(duration);
+        return sb.toString();
+    }
+
+
+    public String randomBuildTime(String startTime, String endTime) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date startDate = sdf.parse(startTime);
+            Date endDate = sdf.parse(endTime);
+
+            if (endDate.getTime() <= startDate.getTime()) return null;
+
+            long randomTS = startDate.getTime() + (long) ((endDate.getTime() - startDate.getTime()) * Math.random());
+
+            Date resultDate = new Date(randomTS);
+            SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String resultTimeString = sdf2.format(resultDate);
+            return resultTimeString;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+
+    public void writeLog(String filePath) {
+        Writer writer = null;
+        try {
+            writer = new OutputStreamWriter(new FileOutputStream(filePath, true), "UTF-8");
+
+            while (true) {
+                Thread.sleep(500);
+                String log = product();
+                writer.write(log + "\n");
+                writer.flush();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args == null || args.length <= 0) return;
+
+        ProductLog productLog = new ProductLog();
+        productLog.initPhone();
+        productLog.product();
+        productLog.writeLog(args[0]);
 
     }
 }
